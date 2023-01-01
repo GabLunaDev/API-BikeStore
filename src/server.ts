3d684475de0d75ab3f158b7473641dab9a58850d
GabLunaDev <gabriellunacv@gmail.com>
2023-01-01 13:59:54 -0300

    Creating log library

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,19 @@ import express from 'express';
 import http from 'http';
 import mongoose from 'mongoose';
 import { config } from './config/config';
+import Logging from './library/Logging';
 
 const router = express();
 
+//Connect to mongoDb
 mongoose
     .connect(config.mongo.url, { retryWrites: true, w: 'majority' })
     .then(() => {
-        console.log('Connected');
+        Logging.info('Connected to Database');
     })
-    .catch((error) => console.log(error));
+    .catch((error) => {
+        Logging.error('Unable to connect: ');
+        Logging.error(error);
+    });
+
+//Only start the server if connect
